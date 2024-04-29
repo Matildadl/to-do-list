8769bede5ddae4fa444b71afd105180192c3f665
Matilda Danas-Leek <mfragetecken@gmail.com>
2024-04-29 13:47:21 +0200

    check the grammar in the text

--- run.py
+++ run.py
@@ -6,46 +6,46 @@ tasks = []
 def addTask():
     new_task = input("Enter the task: ")
     priority = input("Enter the priority: ")
-    tasks.append({"task": new_task, "priority": priority})
-    print(f"Task '{new_task}' added successfully")
+    tasks.append({"Task": new_task, "Priority": priority})
+    print(f"Task '{new_task}' Added Successfully")
 
 
 def listTasks():
     if not tasks:
-        print(Fore.RED + "No tasks" + Style.RESET_ALL)
+        print(Fore.RED + "No Tasks" + Style.RESET_ALL)
     else:
-        print(Fore.YELLOW + "List of tasks" + Style.RESET_ALL)
-        sorted_tasks = sorted(tasks, key=lambda task: task['priority'])
+        print(Fore.YELLOW + "List of Tasks" + Style.RESET_ALL)
+        sorted_tasks = sorted(tasks, key=lambda task: task['Priority'])
         for index, task in enumerate(sorted_tasks):
-            print(Fore.BLUE + f" task #{index}. {task['task']}" +
-                  f" (priority: {task['priority']})" + Style.RESET_ALL)
+            print(Fore.BLUE + f" Task #{index}. {task['Task']}" +
+                  f" (Priority: {task['Priority']})" + Style.RESET_ALL)
 
 
 def deleteTask():
     listTasks()
     try:
-        taskToDelete = int(input("choose the number you want to delete:"))
+        taskToDelete = int(input("Choose the number you want to delete:"))
         if taskToDelete >= 0 and taskToDelete < len(tasks):
             tasks.pop(taskToDelete)
-            print(f"task #{taskToDelete} deleted successfully")
+            print(f"Task #{taskToDelete} Deleted Successfully")
         else:
-            print(f"task #{taskToDelete} does not exist")
+            print(f"Task #{taskToDelete} Does Not Exist")
     except ValueError:
         print("Invalid input: Please enter a valid number.")
     except IndexError:
-        print(f"Invalid input: Task #{taskToDelete} does not exist.")
+        print(f"Invalid input: Task #{taskToDelete} Does Not Exist.")
 
 
 if __name__ == "__main__":
-    print("Welcome to the to do-list")
+    print("Welcome to The To Do-List")
 
     while True:
         print("\n")
-        print("please select one of the following options")
+        print("Please select one of the following options:")
         print("------------------------")
         print("1. Add a task")
-        print("2. delete a task")
-        print("3. list tasks")
+        print("2. Delete a task")
+        print("3. List tasks")
         print("4. Exit")
 
         choice = input("Enter your choice: ")
@@ -61,4 +61,4 @@ if __name__ == "__main__":
         else:
             print("Invalid input, please try again.")
 
-    print("Thank you for using the to do-list")
+    print("Thank you for using The To Do-list")
